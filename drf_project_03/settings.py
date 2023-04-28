9ce5cf265ee9c13c295fe6b7443ae68dc54561c1
louis N <louis_n@naver.com>
2023-04-29 03:43:10 +0900

    create users app

--- drf_project_03/settings.py
+++ drf_project_03/settings.py
@@ -38,7 +38,17 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "users",
 ]
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    )
+}
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
